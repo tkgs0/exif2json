8fc185c003722a530eb171687ed06a4d1260406a
tkgs0 <hgc.sakuya@ya.ru>
2022-11-20 00:36:29 +0800

    commit

---
+++ README.md
@@ -0,0 +1,40 @@
+
+# Exif to json
+
+<div>
+<a href="./LICENSE">
+    <img src="https://img.shields.io/github/license/tkgs0/exif2json.svg" alt="license">
+</a>
+<a href="https://www.python.org">
+    <img src="https://img.shields.io/badge/python-3.8+-blue.svg" alt="python">
+</a>
+<a href="https://pypi.python.org/pypi/Pillow">
+    <img src="https://img.shields.io/badge/PIL-9.+-yellow.svg" alt="Pillow">
+</a>
+<a href="https://pypi.python.org/pypi/ujson">
+    <img src="https://img.shields.io/badge/ujson-5.+-blueviolet.svg" alt="ujson">
+</a>
+
+将图像的exif数据导出到json
+
+# 使用方法
+
+1. 安装 Python 解释器
+2. 配置 Python 环境变量
+3. clone本项目到本地  
+ - 下载本项目, 将项目**整个文件夹**丢到需要解析的图像文件夹下
+4. 安装环境依赖
+ - 使用命令行工具, `cd` 到项目所在文件夹
+ - 输入 `pip install -r requirements.txt` 安装依赖
+
+  ```
+  pip install -r requirements.txt
+  ```
+
+5. 运行
+
+```
+python run.py
+```
+
+**运行成功会在项目文件夹下生成一个 `exif.json` 文件**
---
+++ run.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+from  sys import version_info as vf
+if not (vf.major == 3 and vf.minor >= 8):
+    print('\033[40;33;1mOnly for Python >= 3.8\033[0m')
+    exit(-1)
+
+
+from pathlib import Path
+try:
+    import ujson as json
+except ModuleNotFoundError:
+    import json
+from PIL import Image
+from PIL.ExifTags import TAGS
+import time
+
+
+filepath = (Path(__file__).parent / '..').rglob(r'*.*')
+outpath = Path(__file__).parent / 'exif.json'
+
+
+start = time.time()
+print('loading...')
+x = 0
+n = len(filepath := list(filepath))
+
+content = dict()
+for pic in filepath:
+    x += 1
+    y = int(float(x) / n * 20)
+    print(
+        f"\r[{'='*y}{' '*(20-y)}] {y*5}% {time.time()-start:.2f}s",
+        end=' ', flush=True
+    )
+    try:
+        img = Image.open(pic).getexif()
+    except Exception as e:
+        continue
+    tags = dict()
+    for k,v in img.items():
+        tag = TAGS.get(k,k)
+        if isinstance(v, tuple):
+            v = list(v)
+        elif isinstance(v, bytes):
+            v = v.decode('unicode-escape').replace('\x00', '')
+        elif not isinstance(v, int):
+            v = str(v)
+        tags.update({tag: v})
+    content.update({pic.name: tags})
+
+
+outpath.write_text(
+    json.dumps(content, indent=2, ensure_ascii=False),
+    encoding='utf-8'
+)
+
+print('done.')
